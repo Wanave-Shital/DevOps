359320a5a4282014ad6ae52dee66aebcb4c82af9
Wanave-Shital <53616965+Wanave-Shital@users.noreply.github.com>
2021-03-16 15:35:01 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,9 @@
+create table studentData(Roll_No INT primary key, Name TEXT,Address TEXT,Mobile_Number TEXT,PAN_NUMBER TEXT);
+insert into studentData values(1,'Sheetal','Baramati','7758962280','456789gtAWF12');
+insert into studentData values(2,'Sheela','Nager','7358912180','4QW56799gtF12');
+insert into studentData values(3,'Rohan','Pune','9637855070','4WRG5687F12');
+insert into studentData values(4,'Santosh','Beed','7657812180','12WRGDSgtF12');
+insert into studentData values(5,'Saurabhi','Nager','7358912180','123456jgASVF12');
+
+
+
